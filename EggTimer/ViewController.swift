d694b92f44b2e4ace548e81ed2e135908452d771
Ordaz Romero Sergio <orcheko@gmail.com>
2022-09-01 01:14:55 -0500

    Temporizadores y funciones asincrónicas concluidas

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var totalTime: Int = 0
+    var time: Int = 0
+    var timer: Timer = Timer()
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var progressBar: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
-
+    @IBAction func hardnessSelect(_ sender: UIButton) {
+        let hardness: [String: Int] = ["Soft": 5 , "Medium": 7, "Hard": 12]
+        let timeHardness: Int = hardness[sender.currentTitle!]!
+        time = 60 * timeHardness
+        totalTime = time
+        timer.invalidate()
+        progressBar.alpha = 1
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: timerUpdate)
+    }
+    
+    func timerUpdate(timer: Timer) {
+        if time > 0 {
+            titleLabel.text = "Faltan \(time) segundos!"
+            time -= 1
+            progressBar.progress =  1.0 - (Float(time) / Float(totalTime))
+        } else {
+            titleLabel.text = "Tu huevo ya está listo 😄"
+            timer.invalidate()
+        }
+    }
+    
 }
